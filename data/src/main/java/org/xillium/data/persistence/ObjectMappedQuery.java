cb4f74127d0846687c2522ad7383eb566c7e9eb6
Brian Hu <brianwhu@yahoo.com>
2012-05-07 01:18:05 -0400

    ObjectMappedQuery made friendly to bean assembler

--- data/src/main/java/org/xillium/data/persistence/ObjectMappedQuery.java
+++ data/src/main/java/org/xillium/data/persistence/ObjectMappedQuery.java
@@ -2,6 +2,7 @@ package org.xillium.data.persistence;
 
 import org.xillium.base.beans.Beans;
 import org.xillium.data.*;
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.*;
 import java.lang.reflect.Field;
@@ -34,7 +35,7 @@ public class ObjectMappedQuery<T extends DataObject> extends ParametricQuery {
                             for (int i = 1, ii = meta.getColumnCount()+1; i < ii; ++i) {
                                 try {
                                     String name = Beans.toLowerCamelCase(meta.getColumnName(i), '_');
-                                    list.add(new Column2Field(i, _type.getDeclaredField(name)));
+                                    list.add(new Column2Field(i, _type.getField(name)));
                                 } catch (NoSuchFieldException x) {
                                     // ignored
                                 }
@@ -49,7 +50,38 @@ public class ObjectMappedQuery<T extends DataObject> extends ParametricQuery {
 					T object = _type.newInstance();
 					for (Column2Field c2f: _list) {
 						c2f.field.setAccessible(true);
-						c2f.field.set(object, rs.getObject(c2f.index));
+                        Object value = rs.getObject(c2f.index);
+                        try {
+                            c2f.field.set(object, value);
+                        } catch (IllegalArgumentException x) {
+                            // Oracle && BigDecimal/Timestamp?
+                            if (value instanceof BigDecimal) {
+                                try {
+                                    Class ftype = c2f.field.getType();
+                                    if (Double.class.isAssignableFrom(ftype)) {
+                                        c2f.field.set(object, ((BigDecimal)value).doubleValue());
+                                    } else if (Float.class.isAssignableFrom(ftype)) {
+                                        c2f.field.set(object, ((BigDecimal)value).floatValue());
+                                    } else if (Long.class.isAssignableFrom(ftype)) {
+                                        c2f.field.set(object, ((BigDecimal)value).longValue());
+                                    } else if (Integer.class.isAssignableFrom(ftype)) {
+                                        c2f.field.set(object, ((BigDecimal)value).intValue());
+                                    } else {
+                                        c2f.field.set(object, ((BigDecimal)value).shortValue());
+                                    }
+                                } catch (Throwable t) {
+                                    throw x;
+                                }
+                            } else if (value instanceof java.sql.Timestamp) {
+                                try {
+                                    c2f.field.set(object, new java.sql.Date(((java.sql.Timestamp)value).getTime()));
+                                } catch (Throwable t) {
+                                    throw x;
+                                }
+                            } else {
+                                throw x;
+                            }
+                        }
 					}
 					rows.add(object);
 				}
@@ -68,9 +100,13 @@ public class ObjectMappedQuery<T extends DataObject> extends ParametricQuery {
 		_type = type;
     }
 
-    public ObjectMappedQuery(String parameters,  Class<T> type) throws IllegalArgumentException {
+    public ObjectMappedQuery(String parameters,  String classname) throws IllegalArgumentException {
         super(parameters);
-		_type = type;
+		try {
+            _type = (Class<T>)Class.forName(classname);
+        } catch (ClassNotFoundException x) {
+            throw new IllegalArgumentException(x);
+        }
     }
 
     public List<T> getResults(Connection conn, DataObject object) throws Exception {
