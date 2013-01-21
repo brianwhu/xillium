3e8bc39d1463f9aa44dfbfc741c39f091fed032f
Brian Hu <brianwhu@yahoo.com>
2013-01-21 13:06:47 +0800

    detects dependency loops

--- core/src/main/java/org/xillium/core/util/ModuleSorter.java
+++ core/src/main/java/org/xillium/core/util/ModuleSorter.java
@@ -62,23 +62,39 @@ public class ModuleSorter {
          *  If o1 and o2 are identical, they equal
          *  If o1 is eventually based on o2, then o1 is after o2
          *  If o2 is eventually based on o1, then o2 is before o1
-         *  Otherwise the order is given by their names
+         *  Otherwise the order is given by the dictionary order of their names
+         *
+         * @throws IllegalArgumentException if a dependency loop is detected
          */
         public int compare(Entry o1, Entry o2) {
             if (o1.name.equals(o2.name)) {
                 return 0;
             } else {
                 // is o1 based on o2?
+                Set<String> seen = new HashSet<String>();
+                seen.add(o1.name);
                 Entry superier = _map.get(o1.base);
                 while (superier != null) {
                     if (superier.name.equals(o2.name)) return 1;
+                    if (seen.contains(superier.name)) {
+                        throw new IllegalArgumentException(o1.name + ": loop detected at " + superier.name);
+                    } else {
+                        seen.add(superier.name);
+                    }
                     superier = _map.get(superier.base);
                 }
 
                 // is o2 based on o1?
+                seen.clear();
+                seen.add(o2.name);
                 superier = _map.get(o2.base);
                 while (superier != null) {
                     if (superier.name.equals(o1.name)) return -1;
+                    if (seen.contains(superier.name)) {
+                        throw new IllegalArgumentException(o2.name + ": loop detected at " + superier.name);
+                    } else {
+                        seen.add(superier.name);
+                    }
                     superier = _map.get(superier.base);
                 }
 
