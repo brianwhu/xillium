3a893d5c362d100706017868d7ca6b1f8d403bb7
Brian Hu <brianwhu@yahoo.com>
2014-01-03 04:48:11 +0800

    don't connect till ready to call the sql block

--- tool/src/main/java/org/xillium/tool/data/CallBlock.java
+++ tool/src/main/java/org/xillium/tool/data/CallBlock.java
@@ -21,8 +21,8 @@ import org.xillium.core.conf.StorageConfiguration;
 public class CallBlock {
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
-            System.err.println("Usage: CallableBlocks data-source.xml anonymous-sql-block.sql [ arguments ... ]");
-            System.err.println("Usage: CallableBlocks data-source.xml anonymous-sql-block.xml:name [ arguments ... ]");
+            System.err.println("Usage: CallBlock data-source.xml anonymous-sql-block.sql [ arguments ... ]");
+            System.err.println("Usage: CallBlock data-source.xml anonymous-sql-block.xml:name [ arguments ... ]");
             System.exit(0);
         }
 
@@ -36,9 +36,8 @@ public class CallBlock {
         if (separator > 0) {
             Map<String, ParametricStatement> map = new HashMap<String, ParametricStatement>();
             factory.setBurnedIn(StorageConfiguration.class, map, "-");
-//StorageConfiguration(map, "-") {
             assembler.build(args[1].substring(0, separator+4));
-            ps = map.get(args[1].substring(separator+5));
+            ps = map.get("-/" + args[1].substring(separator+5));
         } else {
             StringBuilder sql = new StringBuilder();
             BufferedReader r = new BufferedReader(new FileReader(args[1]));
@@ -49,11 +48,8 @@ public class CallBlock {
             ps = new ParametricStatement();
             ps.set(sql.toString());
         }
-System.out.println(ps.getSQL());
 
-        Connection conn = dataSource.getConnection();
-
-        Class<? extends DataObject> c = ps.getDataObjectClass("lab.data.persistence.CallableBlocksParams");
+        Class<? extends DataObject> c = ps.getDataObjectClass("org.xillium.tool.data.CallBlocksParams");
         if (c != null) {
             DataObject t = c.newInstance();
             if (args.length == 2) {
@@ -62,24 +58,35 @@ System.out.println(ps.getSQL());
             } else if (args.length == 3 && args[2].indexOf('=') < 0) {
                 t = new org.xillium.data.validation.Dictionary().collect(t, new DataBinder().load(args[2]));
 System.out.println("calling with"); System.out.println(Beans.toString(t));
-                ps.executeProcedure(conn, t);
+                execute(ps, t, dataSource);
 System.out.println("returned with"); System.out.println(Beans.toString(t));
             } else if (args[2].indexOf('=') < 0) {
                 DataBinder binder = new DataBinder().load(args[2]);
                 t = new org.xillium.data.validation.Dictionary().collect(t, binder.load(args, 3));
 System.out.println("calling with"); System.out.println(Beans.toString(t));
-                ps.executeProcedure(conn, t);
+                execute(ps, t, dataSource);
 System.out.println("returned with "); System.out.println(Beans.toString(t));
             } else {
                 t = new org.xillium.data.validation.Dictionary().collect(t, new DataBinder().load(args, 2));
 System.out.println("calling with"); System.out.println(Beans.toString(t));
-                ps.executeProcedure(conn, t);
+                execute(ps, t, dataSource);
 System.out.println("returned with "); System.out.println(Beans.toString(t));
             }
         } else {
-            ps.executeProcedure(conn, (DataObject)null);
+System.out.println("calling w/o parameters");
+            execute(ps, (DataObject)null, dataSource);
         }
+    }
 
-        conn.close();
+    private static final void execute(ParametricStatement ps, DataObject data, DataSource source) throws SQLException {
+        System.err.println("connecting ...");
+        Connection conn = source.getConnection();
+        System.err.println("connected");
+        try {
+            ps.executeProcedure(conn, data);
+        } finally {
+            conn.close();
+            System.err.println("disconnected");
+        }
     }
 }
