90d5a8c48d5b6e468dccc789bb5791bf8c29ebf0
Brian Hu <brianwhu@yahoo.com>
2014-01-09 00:21:41 +0800

    JMXCommander with shorten method names

--- core/src/main/java/org/xillium/core/management/JMXCommander.java
+++ core/src/main/java/org/xillium/core/management/JMXCommander.java
@@ -11,8 +11,17 @@ import org.xillium.data.validation.*;
 
 /**
  * JMXCommander retrieves JMX bean information into a DataBinder.
+ * <ul>
+ * <li>l(list) - list domains and objects</li>
+ * <li>v(view) - view details of an object</li>
+ * <li>g(get)  - get an attribute value</li>
+ * <li>s(set)  - set an attribute value</li>
+ * <li>a(act)  - act upon an operation</li>
+ * </ul>
  */
 public class JMXCommander {
+    private static final String MESSAGE = "jmx.exception";
+
     private final MBeanServer mbs;
     private final Field fld;
     private final DataBinder bdr;
@@ -27,7 +36,7 @@ public class JMXCommander {
         }
     }
 
-    public JMXCommander list() {
+    public JMXCommander l() {
         String domains[] = mbs.getDomains();
         Arrays.sort(domains);
         bdr.put("default", mbs.getDefaultDomain());
@@ -35,69 +44,89 @@ public class JMXCommander {
         return this;
     }
 
-    public JMXCommander list(String selector) throws JMException {
-        Set<ObjectName> names = mbs.queryNames(selector != null ? new ObjectName(selector) : null, null);
-        bdr.put("count", String.valueOf(names.size()));
-        bdr.putResultSet("objects", new CachedResultSet(names, "domain", "canonicalName"));
-        return this;
-    }
-
-    public JMXCommander desc(String selector) throws JMException {
-        MBeanInfo info = mbs.getMBeanInfo(new ObjectName(selector));
-        bdr.put("className", info.getClassName());
-        bdr.put("description", info.getDescription());
-        MBeanAttributeInfo[] attrs = info.getAttributes();
-        bdr.putResultSet("attributes", new CachedResultSet(Arrays.asList(attrs), "name", "description", "type"));
-        MBeanOperationInfo[] opers = info.getOperations();
-        bdr.putResultSet("operations", new CachedResultSet(Arrays.asList(opers), "name", "description", "returnType", "signature"));
-        return this;
-    }
-
-    public JMXCommander get(String selector, String attribute) throws JMException {
-        if (selector != null && attribute != null) {
-            bdr.put("value", String.valueOf(mbs.getAttribute(new ObjectName(selector), attribute)));
-        } else {
-            throw new RuntimeException("MissingObjectPropertyName");
+    public JMXCommander l(String selector) {
+        try {
+            Set<ObjectName> names = mbs.queryNames(selector != null ? new ObjectName(selector) : null, null);
+            bdr.put("count", String.valueOf(names.size()));
+            bdr.putResultSet("objects", new CachedResultSet(names, "domain", "canonicalName"));
+        } catch (Exception x) {
+            bdr.put(MESSAGE, Throwables.getExplanation(x));
         }
         return this;
     }
 
-    public JMXCommander set(String selector, String attribute, String value) throws JMException, DataValidationException {
-        ObjectName name = new ObjectName(selector);
-        for (MBeanAttributeInfo attr: mbs.getMBeanInfo(name).getAttributes()) {
-            if (attr.getName().equals(attribute)) {
-                try {
-                    mbs.setAttribute(name, new Attribute(attribute, new Validator("-", Beans.boxPrimitive(Beans.classForName(attr.getType())), fld).parse(value)));
-                } catch (ClassNotFoundException x) {
-                    throw new DataValidationException(x.getMessage(), x);
+    public JMXCommander v(String selector) {
+        try {
+            MBeanInfo info = mbs.getMBeanInfo(new ObjectName(selector));
+            bdr.put("className", info.getClassName());
+            bdr.put("description", info.getDescription());
+            MBeanAttributeInfo[] attrs = info.getAttributes();
+            bdr.putResultSet("attributes", new CachedResultSet(Arrays.asList(attrs), "name", "description", "type"));
+            MBeanOperationInfo[] opers = info.getOperations();
+            bdr.putResultSet("operations", new CachedResultSet(Arrays.asList(opers), "name", "description", "returnType", "signature"));
+        } catch (Exception x) {
+            bdr.put(MESSAGE, Throwables.getExplanation(x));
+        }
+        return this;
+    }
+
+    public JMXCommander g(String selector, String attribute) {
+        try {
+            bdr.put("value", Beans.toString(mbs.getAttribute(new ObjectName(selector), attribute)));
+        } catch (Exception x) {
+            bdr.put(MESSAGE, Throwables.getExplanation(x));
+        }
+        return this;
+    }
+
+    public JMXCommander s(String selector, String attribute, String value) {
+        try {
+            ObjectName name = new ObjectName(selector);
+            MBeanAttributeInfo attr = null;
+            for (MBeanAttributeInfo info: mbs.getMBeanInfo(name).getAttributes()) {
+                if (info.getName().equals(attribute)) {
+                    attr = info;
+                    break;
                 }
-                break;
             }
+            if (attr != null) {
+                mbs.setAttribute(name, new Attribute(attribute, new Validator("-", Beans.boxPrimitive(Beans.classForName(attr.getType())), fld).parse(value)));
+            } else {
+                throw new AttributeNotFoundException(attribute);
+            }
+        } catch (Exception x) {
+            bdr.put(MESSAGE, Throwables.getExplanation(x));
         }
         return this;
     }
 
-    public JMXCommander act(String selector, String operation, String... args) throws JMException, DataValidationException {
-        ObjectName name = new ObjectName(selector);
-        for (MBeanOperationInfo oper: mbs.getMBeanInfo(name).getOperations()) {
-            if (oper.getName().equals(operation)) {
+    public JMXCommander a(String selector, String operation, String... args) {
+        try {
+            ObjectName name = new ObjectName(selector);
+            MBeanOperationInfo oper = null;
+            for (MBeanOperationInfo info: mbs.getMBeanInfo(name).getOperations()) {
+                if (info.getName().equals(operation)) {
+                    oper = info;
+                    break;
+                }
+            }
+            if (oper != null) {
                 MBeanParameterInfo[] params = oper.getSignature();
                 if (params.length != args.length) {
-                    throw new RuntimeException("IncorrectNumberOfArguments");
+                    throw new IllegalArgumentException("IncorrectNumberOfArguments");
                 }
                 Object[] arguments = new Object[params.length];
                 String[] signature = new String[params.length];
                 for (int i = 0; i < params.length; ++i) {
                     signature[i] = params[i].getType();
-                    try {
-                        arguments[i] = new Validator("-", Beans.boxPrimitive(Beans.classForName(signature[i])), fld).parse(args[i]);
-                    } catch (ClassNotFoundException x) {
-                        throw new DataValidationException(x.getMessage(), x);
-                    }
+                    arguments[i] = new Validator("-", Beans.boxPrimitive(Beans.classForName(signature[i])), fld).parse(args[i]);
                 }
-                bdr.put("value", String.valueOf(mbs.invoke(name, operation, arguments, signature)));
-                break;
+                bdr.put("value", Beans.toString(mbs.invoke(name, operation, arguments, signature)));
+            } else {
+                throw new ServiceNotFoundException(operation);
             }
+        } catch (Exception x) {
+            bdr.put(MESSAGE, Throwables.getExplanation(x));
         }
         return this;
     }
