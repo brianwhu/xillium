2c0f8edefc8367c52e69822f4080f2c890292776
Brian Hu <brianwhu@yahoo.com>
2014-04-08 14:15:04 +0800

    getKnownClasses() moved to src/main/java/org/xillium/tool/Command.java

--- tool/src/main/java/org/xillium/tool/Command.java
+++ tool/src/main/java/org/xillium/tool/Command.java
@@ -1,7 +1,11 @@
 package org.xillium.tool;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URISyntaxException;
 import java.util.*;
-import org.xillium.base.beans.Beans;
+import java.util.jar.*;
 
 
 /**
@@ -15,7 +19,7 @@ public class Command {
             System.err.println("Usage: xillium-tool COMMAND [ ARGUMENTS ... ]");
             System.err.println("       xillium-tool --list");
         } else if (args.length == 1 && args[0].equals("--list")) {
-            for (Class<?> c: Beans.getKnownClasses(PACKAGE)) {
+            for (Class<?> c: getKnownClasses(PACKAGE)) {
                 if (c == Command.class) continue;
                 System.out.println(c.getName().replace(PACKAGE + '.', "\t"));
             }
@@ -30,6 +34,57 @@ public class Command {
                 command.getMethod("main", String[].class).invoke(null, (Object)Arrays.copyOfRange(args, 1, args.length));
             }
         }
+    }
+
+    public static List<Class<?>> getKnownClasses(String pkgname) throws IllegalArgumentException, IOException {
+        List<Class<?>> classes = new ArrayList<Class<?>>();
+
+        String path = pkgname.replace('.', '/');
+
+        URL resource = ClassLoader.getSystemClassLoader().getResource(path);
+        if (resource == null) {
+            throw new IllegalArgumentException("Unknown package: " + pkgname);
+        }
+
+        File directory = null;
+        try {
+            directory = new File(resource.toURI());
+        } catch (Exception x) {
+            directory = null;
+        }
+
+        if (directory != null && directory.exists()) {
+            String[] files = directory.list();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].endsWith(".class")) {
+                    try {
+                        classes.add(Class.forName(pkgname + '.' + files[i].substring(0, files[i].length() - 6)));
+                    } catch (ClassNotFoundException x) {
+                        //ignore
+                    }
+                }
+            }
+        } else {
+            JarFile jar = new JarFile(resource.getFile().replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", ""));
+            try {
+                for (Enumeration<JarEntry> entries = jar.entries(); entries.hasMoreElements();) {
+                    JarEntry entry = entries.nextElement();
+                    String name = entry.getName();
+                    if (name.startsWith(path) && name.length() > (path.length() + "/".length())) {
+                        if (entry.isDirectory()) continue;
+                        try {
+                            classes.add(Class.forName(name.substring(0, name.length() - 6).replace('/', '.').replace('\\', '.')));
+                        } catch (ClassNotFoundException x) {
+                            //ignore
+                        }
+                    }
+                }
+            } finally {
+                jar.close();
+            }
+        }
+
+        return classes;
     }
 }
 
