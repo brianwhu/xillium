31e42583c89f706d779f81423f5d07841b3f693c
Brian Hu <brianwhu@yahoo.com>
2014-07-29 13:54:20 +0800

    documentation update

--- core/src/main/java/org/xillium/core/DynamicService.java
+++ core/src/main/java/org/xillium/core/DynamicService.java
@@ -7,8 +7,9 @@ import org.xillium.data.*;
  * A DynamicService interface that exposes its request interface via a public method.
  */
 public interface DynamicService extends Service {
-	/**
-	 * Returns the request object type of this service. This method should never return null.
-	 */
-	public Class<? extends DataObject> getRequestType();
+    /**
+     * Returns the request type of this service, an implementation of DataObject. This method should 
+     * never return null. Use DataObject.Empty.class to represent a request type with no parameters.
+     */
+    public Class<? extends DataObject> getRequestType();
 }
